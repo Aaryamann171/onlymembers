6670fbf5d69ae9b445b7358788f8ebac9edb9744
Aaryamann171 <aaryamann171@gmail.com>
2020-11-01 17:09:24 +0530

    made changes to web app

--- hello/views.py
+++ hello/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 
 
 def hello(request):
-    return HttpResponse("hello, world!")
+    users = User.objects.all()
+    return render(request, "home.html", {'users': users})
