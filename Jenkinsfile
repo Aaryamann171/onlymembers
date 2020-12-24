310934b55327c464fabcc2fe4681bcd8277afb89
Aaryamann171 <aaryamann171@gmail.com>
2020-12-24 09:36:57 +0530

    added members model, option to add members, made necessary changes in the views file, added forms.py, added base.html for modular templates, added navbar, minor ui tweaks

--- Jenkinsfile
+++ Jenkinsfile
@@ -10,7 +10,7 @@ node {
     stage('Build image') {
         /* This builds the actual image */
 
-        app = docker.build("aaryamann171/test")
+        app = docker.build("aaryamann171/onlymembers")
     }
 
     stage('Test image') {
--- hello/views.py
+++ hello/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
-
+from .models import Member
+from .forms import MemberForm
 
 def hello(request):
     users = User.objects.all()
-    return render(request, "home.html", {'users': users})
+    members = [member.username for member in Member.objects.all()]
+    return render(request, "home.html", {'users': users, 'members': members})
+
+
+def member_create_view(request):
+    form = MemberForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+
+    context = {
+        'form': form
+    }
+    return render(request, "create_member.html", context)
